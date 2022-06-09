79db35d8db1ff2ab54fd93181be2c0d3b10f0e91
Nolan Jaeger <71898843+Nolan-Jgr@users.noreply.github.com>
2022-06-09 12:19:25 -0400

    Update Counter.java

--- Counter.java
+++ Counter.java
@@ -1,4 +1,5 @@
 import java.util.*;
+// a counter object with a value and an increment function
 public class Counter
 {
    private int value;
@@ -15,12 +16,15 @@ public class Counter
       return value;
    }
 }
+// a coinToss game that uses the counter object to count the number of heads and tails 
 class coinToss
 {
    public static void main(String[]args)
    {
      Counter heads = new Counter();
      Counter tails = new Counter();
+     // flips a coin 100 times and either increments the heads or tails counter based on the outcome
+     // 1 means a tail and 0 means heads
      for(int i = 1; i <= 100; i++)
      {
       int toss = (int)(Math.random()* 2);
@@ -32,7 +36,8 @@ class coinToss
       {
          heads.increment();
       }
-     } 
+     }
+     // prints the outcome of the counters
      System.out.println("Heads: " + heads.getValue());
      System.out.println("Tails: " + tails.getValue());
    }
