0e1fc62ffddf94390fb94ccbdcbabe073278e93a
Nolan Jaeger <71898843+Nolan-Jgr@users.noreply.github.com>
2022-06-09 12:15:33 -0400

    Update Circles.java

--- Circles.java
+++ Circles.java
@@ -4,16 +4,20 @@ public class Circles
    public static void main(String[]args)
    {
       Scanner scan = new Scanner(System.in);
+      // asks for the values of the first circle
       System.out.println("Enter the center values(X1 & Y1) and the radius for circle 1");
       int X1 = scan.nextInt();
       int Y1 = scan.nextInt();
       int R1 = scan.nextInt();
+      // asks for the values of the second circle 
       System.out.println("Enter the center values(X2 & Y2) and the radius for circle 2");
       int X2 = scan.nextInt();
       int Y2 = scan.nextInt();
       int R2 = scan.nextInt();
       String judgement = "";
       double distance = Math.sqrt(Math.pow(X2-X1,2) + Math.pow(Y2-Y1,2));
+      // determines if the two circles are either inside on another or overlapping 
+      // assigns judgment statement to variable
       if(distance >= (R1 + R2))
       {
          judgement += "circle 2 is completely outside circle 1";
