c32b26cb09545dcbe3479da2eb55b1632012be0c
Nolan Jaeger <71898843+Nolan-Jgr@users.noreply.github.com>
2022-08-23 14:48:13 -0400

    Update MinMaxAvg.java

--- MinMaxAvg.java
+++ MinMaxAvg.java
@@ -1,9 +1,12 @@
 import java.util.*;
+// this program uses a 4x4 array of random values that represent grades to 
+// calculate the min grade, max grade, and avg of the grades
 public class MinMaxAvg
 {
    public static void main(String[]args)
    {
       int[][] Grades = new int[4][4];
+      // the array is populated with random values
       for(int rows = 0; rows < Grades.length; rows++)
       {
          for(int col = 0; col < Grades.length; col++)
@@ -11,6 +14,7 @@ public class MinMaxAvg
             Grades[rows][col] = (int)(Math.random()*100) + 1;
          }
       }
+      // the array of grades is printed
       System.out.println("Array Grades: ");
       for(int rows = 0; rows < Grades.length; rows++)
       {
@@ -20,13 +24,18 @@ public class MinMaxAvg
          }
          System.out.println();
       }
+      // a function call is made using the array of grades
       minMaxAvg(Grades);
    }
+   // this function takes in a 2D array and determines the min grade, max grade, and avg grade
    public static void minMaxAvg(int[][] a)
    {
+      // base values for min max and avg are set
       int min = a[0][0];
       int max = a[0][0];
       double avg = 0;
+      // depending of the value of grade at the specified index the min and max are set
+      // all grade values are added to the avg variable
       for(int r = 0; r < a.length;r++)
       {
          for(int c = 0; c < a.length;c++)
@@ -42,6 +51,7 @@ public class MinMaxAvg
             avg += a[r][c];
          }
       }
+      // avg is calcualted and the grade info is printed 
       avg = avg/16.0;
       System.out.println("Highest grade: " + max);
       System.out.println("Lowest grade: " + min);
