58ba7566709e3c49364fa3cddcd337520d463fcc
Nolan Jaeger <71898843+Nolan-Jgr@users.noreply.github.com>
2022-08-23 15:02:19 -0400

    Update Occurences.java

--- Occurences.java
+++ Occurences.java
@@ -1,4 +1,6 @@
 import java.util.*;
+// this program asks the user to populate two 3x3 arrays
+// the two arrays are then checked to see if they are the same
 public class Occurences
 {
    public static void main(String[] args)
@@ -8,6 +10,7 @@ public class Occurences
       int[][] y = new int[3][3];
       System.out.println("Enter 9 values for each of the 3x3 arrays");
       System.out.println("Array A: ");
+      // the user populates arrat A
       for(int r =0; r < x.length; r++)
       {
          for(int c =0; c < x[0].length; c++)
@@ -16,6 +19,7 @@ public class Occurences
          }
       }
       System.out.println("Array B: ");
+      // the user populates array B
       for(int r =0; r < y.length; r++)
       {
          for(int c =0; c < y[0].length; c++)
@@ -23,6 +27,7 @@ public class Occurences
             y[r][c] = scan.nextInt();  
          }
       }
+      // a function call to isEquivalent is made to check is the two arrays are the same
       if(isEquivalent(x, y))
       {
          System.out.println("Judgment: The arrays are equivalent");
@@ -32,11 +37,13 @@ public class Occurences
          System.out.println("Judgement: The arrays are not equivalent");
       }
    }
+   // this function takes in two 2D arrays and determines if the two arrays are the same
    static boolean isEquivalent(int[][] a, int[][] b)
    {
       int[] one = new int[a.length * a[0].length];
       int[] two = new int[b.length * b[0].length];
       int index = 0;
+      // takes all the values in the first array and puts them in a single 1D array
       for(int r = 0; r < a.length; r++)
       {
          for(int c = 0; c < a[0].length; c++)
@@ -46,6 +53,7 @@ public class Occurences
          }
       }
       index = 0;
+      // takes all the values in the second array and puts them in a single 1D array
       for(int r = 0; r < b.length; r++)
       {
          for(int c = 0; c < b[0].length; c++)
@@ -54,6 +62,7 @@ public class Occurences
             index++;   
          }
       }
+      // the two arrays are sorted in ascending order
       for(int i=0; i < one.length-1;i++)
       {
          for(int j = i+1; j < one.length; j++)
@@ -72,6 +81,8 @@ public class Occurences
             }
          }
       }
+      // the two sorted arrays are then checked index by index to determine if
+      // the inputed arrays are the same
       for(int i = 0; i < one.length; i++)
       {
          if(one[i] != two[i])
