aaf1ce5cf54c431406d791d0a9dd847ce63c6c7d
Nolan Jaeger <71898843+Nolan-Jgr@users.noreply.github.com>
2022-08-23 14:38:31 -0400

    Update IndexOfLargest.java

--- IndexOfLargest.java
+++ IndexOfLargest.java
@@ -1,4 +1,5 @@
 import java.util.*;
+// this program asks the user for 10 integers and returns the index of the largest integer
 public class IndexOfLargest
 {
    public static void main(String[]args)
@@ -6,6 +7,7 @@ public class IndexOfLargest
       Scanner scan = new Scanner(System.in);
       System.out.println("Enter 10 integers");
       int[] x = new int[10];
+      // the array is filled with user inputed integers
       for(int i = 0;i < x.length;i++)
       {
          x[i] = scan.nextInt();
@@ -16,8 +18,10 @@ public class IndexOfLargest
          System.out.print(i + " ");
       }
       System.out.println();
+      // calls the findIndex function and prints the index of the largest value
       System.out.print("Index of largest value is " + findIndex(x));
    }
+   // this function take in an array and returns the index of the largest integer within that array
    public static int findIndex(int[] a)
    {
       int temp = a[0];
