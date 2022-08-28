42fae18a7c71d1a28edcc8af9e3f404a00c637d1
Nolan Jaeger <71898843+Nolan-Jgr@users.noreply.github.com>
2022-08-28 16:43:35 -0400

    Update PhoneBill.java

--- PhoneBill.java
+++ PhoneBill.java
@@ -1,13 +1,16 @@
 import java.util.*;
+// this program represents a phone bill either a regualar plan or premium plan
 public class PhoneBill
 {
    public static void main(String[]args)
    {
       Scanner scan = new Scanner(System.in);
+      // account num is entered and the type of plan is specified
       System.out.println("enter an account number and service code(r/R or p/P)");
       int account = scan.nextInt();
       char code = scan.next().charAt(0);
       double amount = 0.0;
+      // if the plan is regular the total minutes is entered and the amount is calculated
       if(code == 'r' || code == 'R') 
       {
          System.out.println("enter the number of minutes used");
@@ -24,14 +27,13 @@ public class PhoneBill
          {
             amount = 0.5*(min-50) + 15.0; 
          }
+         // account information is printed
          System.out.println("Account number: " + account);
          System.out.println("Service type: Regular");
          System.out.println("Total minutes: " + min);
          System.out.println("Amount due: $" + amount);
-        
-         
-         
       }
+      // if the plan is premium daytime and nightime min are specified and the amounts are calculated separetly 
       else if(code == 'p' || code == 'P')
       {
          System.out.println("enter the number of minutes for daytime use and nighttime use");
@@ -46,6 +48,7 @@ public class PhoneBill
          {
             amount += 0.1*(nmin - 100);
          }
+         // account information is printed
          System.out.println("Account number: " + account);
          System.out.println("Service type: Premium");
          System.out.println("Daytime minutes: " + dmin);
