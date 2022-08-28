74b906bb68fa21ea645852a0d6ebeb142e4620de
Nolan Jaeger <71898843+Nolan-Jgr@users.noreply.github.com>
2022-08-28 16:38:12 -0400

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,14 +1,18 @@
 import java.util.*;
+// this program uses loops to print a patter
 public class Pattern
 {
    public static void main(String[]args)
    {
+      // number of rows
       for(int x = 6; x > 0; x--)
       {
+         // adds needed white space
          for(int y = 0; y < 6-x;y++)
          {
             System.out.print("  ");
          }
+         // prints the data that creates the pattern
          for(int z = 1; z <= x; z++)
          {
             System.out.print(z + " ");
